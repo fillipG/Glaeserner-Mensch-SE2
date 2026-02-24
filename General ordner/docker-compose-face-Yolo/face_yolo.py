e048e54452be5442de5189df2e7528f0e6f584d4
Jonas <105271972+JonasHWL@users.noreply.github.com>
2026-02-24 22:21:16 +0100

    Aufräumen

--- General ordner/docker-compose-face-Yolo/face_yolo.py
+++ General ordner/docker-compose-face-Yolo/face_yolo.py
@@ -11,11 +11,6 @@ INPUT_DIR = "main_image"
 confidence = 0.7  # Ab welcher Konfidenz ein Gesicht erkannt wird
 padding = 40     # Zusätzlicher Rand, verbessert das entfernen des Hintergrunds.
 
-
-# Ausgabe-Ordner erstellen
-#if not os.path.exists("faces_yolo"):
-#    os.makedirs("faces_yolo")
-
 # YOLO-Modell laden (einmalig, außerhalb der Schleife)
 model = YOLO("yolov8n-face.pt")  # Yolo-face Modell
 
@@ -23,7 +18,6 @@ print("Warte auf Bilder im Ordner 'main_image'... ")
 
 # Dauerhaft den Ordner überwachen
 while True:
-
     # Bild im Ordner suchen
     image_files = [
         f for f in os.listdir(INPUT_DIR)
